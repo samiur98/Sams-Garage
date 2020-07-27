155d252436ba779800f36299676357afa853d4d5
samiur98 <sam@Shahs-MacBook-Pro.local>
2020-07-26 21:52:26 -0700

    Setup for frontend/client-side code complete

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Teddy Bear</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
